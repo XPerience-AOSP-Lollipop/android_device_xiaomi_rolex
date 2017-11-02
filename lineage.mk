3397672a9cbaa90001b73118baece0107240985e
carlosarriagacm <carlosarriagacm@gmail.com>
2017-11-02 13:20:07 -0600

    rolex: don't have supported Fingerprint

--- lineage.mk
+++ lineage.mk
@@ -26,4 +26,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=Xiaomi/rolex/rolex:6.0.1/MMB29M/V8.5.1.0.MCCMIED:user/release-keys \
-PRIVATE_BUILD_DESC="rolex-user 6.0.1 MMB29M V8.5.1.0.MCCMIED release-keys"
+    PRIVATE_BUILD_DESC="rolex-user 6.0.1 MMB29M V8.5.1.0.MCCMIED release-keys"
