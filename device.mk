fd2a5f5c612b447a0fa17ea3113f83721d41aeb5
carlosarriagacm <carlosarriagacm@gmail.com>
2017-11-02 13:13:47 -0600

    rolex: move files to prebuilt folder

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -301,5 +301,5 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
 	ro.allow.mock.location=1 \
 	ro.debuggable=1 \
 	ro.adb.secure=0 \
-        ro.oem_unlock_supported=1
+    ro.oem_unlock_supported=1
 
